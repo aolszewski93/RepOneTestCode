35252119235d2231c8942a16205ab7762fc8ab0c
aolszewski <al.m.olszewski@gmail.com>
2019-12-13 16:04:36 -0500

    code was adjusted so the reps are the same as desired testing reps

--- RepOneStepperWithProximityWithQAChecks/RepOneStepperWithProximityWithQAChecks.ino
+++ RepOneStepperWithProximityWithQAChecks/RepOneStepperWithProximityWithQAChecks.ino
@@ -1,4 +1,7 @@
 /*
+ * Author: Albert Michael Olszewski
+ * Company: RepOne
+ * Date: 12/13/2019
   Stepper Motor with Proximity Sensor for durability tests on the RepOne Strength unit.
   The test fixture this code programs uses a NEMA 17 stepper motor from stepper online,
   a DM542T Digital Stepping Driver, a Twidec 10mm Proximity Sensor, and an Arduino Uno. 
@@ -7,6 +10,7 @@
 
   When the user runs this program, it should pull the tether of the RepOne Sensor around 7ft, 
   release the tether, and then retrieve it. 
+
 */
  
 // Define pins
@@ -19,12 +23,12 @@ int slidePin = 10;   // input for the slide switch that selects what type of pro
  
 // Variables
 
-int pd = 500;       // Pulse Delay period
+int pd = 200;       // Pulse Delay period
 boolean setdir = LOW; // Set Direction
 int stepsPerRev = 800; // this is set on the digital stepper driver
 //int diameterOfWheel = 0.5; // the diameter of the wheel is around 0.5 ft
 //int lengthOfRetraction = 6; // we want the RepOne Sensor 6 ft away from test fixture.
-int driverPosition = (stepsPerRev * 3.8) ; // set the number of steps the motor has to make to return to RepOne sensor. 
+int driverPosition = (stepsPerRev * 4) ; // set the number of steps the motor has to make to return to RepOne sensor. 
 // 
 // 
 
@@ -39,15 +43,15 @@ void tetherFishing(int numReps, int i = 0){
     if(val != 1){
       i++;
       Serial.println("Return to Base.");  //print what program is doing
-      Serial.println("Cycle: "); 
-      Serial.print(i);
+      Serial.print("Cycle: "); 
+      Serial.println(i);
       // run a for loop that steps the motor to where the module being tested is.
       for(int steps = 0; steps < driverPosition; steps++){
         digitalWrite(driverDIR,!setdir); //change direction of the motor
         digitalWrite(driverPUL,HIGH);
-        delayMicroseconds(pd);
+        delayMicroseconds(pd*10);
         digitalWrite(driverPUL,LOW);
-        delayMicroseconds(pd);
+        delayMicroseconds(pd*10);
       }
       
   //    delay(100000000);
@@ -76,8 +80,7 @@ void userCheck(){
   }
   Serial.println(j);
   delay(5000);
-//    j = digitalRead(switchPin);
-//    Serial.println(j);
+
 }
 
 
@@ -99,23 +102,35 @@ void loop() {
 
   //if switch returns 1 run drop test else run lift test
   if(prog == 1){
+    // run drop test with user checks at 10, 50, 200, and 500 reps
     Serial.println("Running Drop Test");
-    tetherFishing(1);
+    tetherFishing(1); //run 1 test rep
     userCheck();
-    tetherFishing(2);
+    tetherFishing(10);
     userCheck();
-    tetherFishing(3);
+    tetherFishing(50);
+    userCheck();
+    tetherFishing(200);
+    userCheck();
+    tetherFishing(500);
     delay(500);
   }
   else{
-    Serial.println("Running Lift Test");
+    // run life testing with checks after 1000, 20000, 100000, and 300000 reps
+    Serial.println("Running Life Test");
+    tetherFishing(1); //run one test rep
+    userCheck();
+    tetherFishing(1000);
+    userCheck();
+    tetherFishing(20000);
+    userCheck();
+    tetherFishing(100000);
+    userCheck();
+    tetherFishing(300000);
     delay(500);
   }
   
-//  userCheck();
-//  tetherFishing(200);
-//  userCheck();
-//  tetherFishing(500);
+
   
   
 }
